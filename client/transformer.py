3767b8454a7a613c96f4f497c7099da6a92a9d9a
Tamara <tammiricki@gmail.com>
2024-02-17 13:07:29 +0100

    add fillna and change CustId to int

--- client/transformer.py
+++ client/transformer.py
@@ -1,14 +1,25 @@
 
-#import json
+
 import numpy as np
 import pandas as pd 
 
 #with transformer file we transform csv file to json 
 #This is just a sample of 10k data 
 df = pd.read_csv('../project_data.csv')
+df.info()
+
+
+#df.isna().sum()
+df['CustomerID'].fillna(0, inplace=True)
+df.isna().sum()
+
+
+df['CustomerID'] = df['CustomerID'].astype('int32')
+
+
+
 
 #add json column in df, splitlines  this splits into multiple rows 
-
 df['json'] = df.to_json(orient='records', lines=True).splitlines()
 
 dfjson = df['json']
@@ -17,3 +28,6 @@ dfjson = df['json']
 
 np.savetxt(r'./output.txt', dfjson.values, fmt='%s')
 #print(df.sample(6))
+
+
+
